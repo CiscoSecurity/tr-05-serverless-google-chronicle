9dc48233fe4ded8bf43f0e82085fbf619919a867
Denys Kulykov <dkuly@softserveinc.com>
2020-03-31 16:55:45 +0300

    remove test for IP

--- tests/functional/tests/test_sightings.py
+++ tests/functional/tests/test_sightings.py
@@ -2,32 +2,6 @@ from ctrlibrary.core.utils import get_observables
 from ctrlibrary.threatresponse.enrich import enrich_observe_observables
 
 
-def test_positive_sighting_ip(module_headers):
-    """Perform testing for enrich observe observables endpoint to get
-    sightings for observable with ip from Chronicle Backstory
-
-    ID: CCTRI-883-666472f6-db34-4240-8eaf-8fc5347253a5
-
-    Steps:
-        1. Send request to enrich deliberate observable endpoint
-
-    Expectedresults:
-        1. Check that data in response body contains expected sightings for
-            observable from Chronicle Backstory
-
-    Importance: Critical
-    """
-    observable = {'type': 'ip', 'value': '1.1.1.1'}
-    response = enrich_observe_observables(
-        payload=[observable],
-        **{'headers': module_headers}
-    )['data']
-    sightings = get_observables(
-        response, 'Chronicle Backstory')['data']['sightings']
-    assert sightings['count'] == 0
-    assert sightings['docs'] == []
-
-
 def test_positive_sighting_domain(module_headers):
     """Perform testing for enrich observe observables endpoint to get
     sightings for observable with domain from Chronicle Backstory
