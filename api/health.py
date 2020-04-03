22f8c18960688339a57f163403e2017dbecc777c
ablahun <ablahun@cisco.com>
2020-04-03 12:34:40 +0300

    CCTRI-935: Refactor health endpoint.

--- api/health.py
+++ api/health.py
@@ -1,11 +1,10 @@
 from flask import Blueprint, current_app
 
-from api.client import TimeFilter, ChronicleClient
+from api.client import ChronicleClient
 from api.utils import (
     jsonify_data,
     get_chronicle_http_client,
-    get_jwt,
-    join_url
+    get_jwt
 )
 
 health_api = Blueprint('health', __name__)
@@ -13,13 +12,6 @@ health_api = Blueprint('health', __name__)
 
 @health_api.route('/health', methods=['POST'])
 def health():
-    url = join_url(
-        current_app.config['API_URL'],
-        '/artifact/listassets'
-        '?artifact.domain_name=www.google.com&page_size=1'
-        f'{str(TimeFilter())}'
-    )
-
     http_client = get_chronicle_http_client(get_jwt())
     chronicle_client = ChronicleClient(current_app.config['API_URL'],
                                        http_client)
