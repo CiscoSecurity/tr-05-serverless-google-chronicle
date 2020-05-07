b0484037b35c9b1497f43bb8b63f2a7da6d4bc7e
oshtaier <alexshtayer@yahoo.com>
2020-05-07 12:01:55 +0300

    Add validation for hash type

--- tests/functional/tests/test_sightings.py
+++ tests/functional/tests/test_sightings.py
@@ -6,7 +6,7 @@ def test_positive_sighting_domain(module_headers):
     """Perform testing for enrich observe observables endpoint to get
     sightings for observable with domain from Chronicle Backstory
 
-    ID: CCTRI-883-13cda7fd-1357-4621-98f6-a0dd3789c3cf
+    ID: CCTRI-768-13cda7fd-1357-4621-98f6-a0dd3789c3cf
 
     Steps:
         1. Send request to enrich observe observable endpoint
@@ -64,7 +64,7 @@ def test_positive_sighting_ip(module_headers):
     """Perform testing for enrich observe observables endpoint to get
     sightings for observable with IP from Chronicle Backstory
 
-    ID: CCTRI-883-33cda5fd-1557-4625-98f5-a0dd3589c35a
+    ID: CCTRI-768-33cda5fd-1557-4625-98f5-a0dd3589c35a
 
     Steps:
         1. Send request to enrich observe observable endpoint
@@ -108,3 +108,45 @@ def test_positive_sighting_ip(module_headers):
         assert relation['related'] == observable[0]
         assert 'value' in relation['source']
         assert 'type' in relation['source']
+
+
+def test_positive_sighting_hash(module_headers):
+    """Perform testing for enrich observe observables endpoint to get
+    sightings for observable by md5 hash type from Chronicle Backstory module
+
+    ID: CCTRI-768-070314bd-d775-4463-94eb-f74cc3886384
+
+    Steps:
+        1. Send request to enrich observe observable endpoint
+
+    Expectedresults:
+        1. Check that data in response body contains expected sightings for
+            observable from Chronicle Backstory
+
+    Importance: Critical
+    """
+    observable = [{'type': 'md5', 'value': '34d5ea586a61b0aba512c0cb1d3d8b15'}]
+    response = enrich_observe_observables(
+        payload=observable,
+        **{'headers': module_headers}
+    )
+    sightings = get_observables(
+        response['data'], 'Chronicle Backstory')['data']['sightings']
+    assert sightings['count'] == 10
+
+    for sighting in sightings['docs']:
+        assert sighting['confidence'] == 'High'
+        assert sighting['count']
+        assert sighting['id']
+        assert 'start_time' in sighting['observed_time']
+        assert 'schema_version' in sighting
+        assert sighting['type'] == 'sighting'
+        assert sighting['source'] == 'Chronicle'
+        assert sighting['title'] == 'Found in Chronicle'
+        assert len(sighting['observables']) == 1
+        assert sighting['observables'] == observable
+
+        for target in sighting['targets']:
+            assert target['type'] == 'endpoint'
+            assert target['observables'][0]['type'] == 'hostname'
+            assert 'start_time' in target['observed_time']
