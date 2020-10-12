78ee24c72c9377238ff746b52a2adad74a3cec5c
MaxMhonc <mhonc@softserveinc.com>
2020-10-12 18:53:40 +0300

    trying to find domains

--- tests/functional/tests/test_relationship.py
+++ tests/functional/tests/test_relationship.py
@@ -10,7 +10,7 @@ from tests.functional.tests.constants import (
 @pytest.mark.parametrize(
     'observable_type, observable',
     (
-     #('ip', '1.1.1.1'),
+     ('ip', 'urirmpyp.top'),
      ('domain', 'urirmpyp.top')
      )
 )
