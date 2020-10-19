1b3f32688cad890c7c362f8254d19a9c583c8e12
MaxMhonc <mhonc@softserveinc.com>
2020-10-19 11:08:49 +0300

    fix in conftest.py valid_jwt().

--- tests/unit/conftest.py
+++ tests/unit/conftest.py
@@ -140,6 +140,7 @@ def valid_jwt(secret_key):
         "project_id": "<PROJECT_ID>",
         "private_key_id": "<PRIVATE_KEY_ID>",
         "private_key": "<PRIVATE_KEY>",
+        "client_email": "<CLIENT_EMAIL>",
         "client_id": "<CLIENT_ID>",
         "auth_uri": "<AUTH_URI>",
         "token_uri": "<TOKEN_URI>",
